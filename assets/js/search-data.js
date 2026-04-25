75900a5c9ccb4c5ed04b871976bcadbca09d281e
orionchen314 <orionchen314@gmail.com>
2026-04-25 10:04:14 +0000

    Deploying to gh-pages from @ orionchen314/orionchen314.github.io@9469ee5c31933fcef4bbdb151788cea68e3d0d19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,15 +436,15 @@ ninja.data = [{
             },},{id: "news-apr-2026-finalized-pmt-waveform-analysis-and-charge-reconstruction-framework",
           title: 'Apr 2026: Finalized PMT waveform analysis and charge reconstruction framework.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-cupec-competition-achievement",
+          title: 'CUPEC Competition Achievement',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
